8d2a00d8bd58f5ece982357122a17d306fe0fd22
Vicheanak <vicheanak@gmail.com>
2018-01-25 17:37:52 +0700

    Initial Commit

---
+++ Konleng Real Estate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Konleng Real Estate
+//
+//  Created by MacBooK  on 1/25/18.
+//  Copyright © 2018 Vicheanak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
